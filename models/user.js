f9554955c535f948badd39742d599fa622478515
neesafarza <aneesazafri@gmail.com>
2020-08-24 16:29:19 +0100

    refactor: change seeding to match db

--- models/user.js
+++ models/user.js
@@ -46,6 +46,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
     
   });
 
